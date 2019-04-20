813fb3c981607633c8fcee076254bca8c27116ce
Zachary <syyun@snu.ac.kr>
2019-04-20 18:09:40 +0900

    add case of latent 1

--- trains/ae.py
+++ trains/ae.py
@@ -1,6 +1,7 @@
 import os
 import time
 import torch
+import logging
 import argparse
 import pandas as pd
 import seaborn as sns
@@ -13,7 +14,7 @@ from collections import defaultdict
 from models.ae import AE
 
 
-def main(args):
+def main(args, ts, logPath):
 
     torch.manual_seed(args.seed)
     if torch.cuda.is_available():
@@ -21,10 +22,8 @@ def main(args):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    ts = time.time()
-
     dataset = MNIST(
-        root='ae/data', train=True, transform=transforms.ToTensor(),
+        root='../data', train=True, transform=transforms.ToTensor(),
         download=True)
     data_loader = DataLoader(
         dataset=dataset, batch_size=args.batch_size, shuffle=True)
@@ -63,7 +62,10 @@ def main(args):
             for i, yi in enumerate(y):
                 id = len(tracker_epoch)
                 tracker_epoch[id]['x'] = z[i, 0].item()
-                tracker_epoch[id]['y'] = z[i, 1].item()
+                if z.shape[1] != 1:
+                    tracker_epoch[id]['y'] = z[i, 1].item()
+                else:
+                    tracker_epoch[id]['y'] = 0
                 tracker_epoch[id]['label'] = yi.item()
 
             loss = loss_fn(recon_x, x)
@@ -75,8 +77,12 @@ def main(args):
             logs['loss'].append(loss.item())
 
             if iteration % args.print_every == 0 or iteration == len(data_loader)-1:
-                print("Epoch {:02d}/{:02d} Batch {:04d}/{:d}, Loss {:9.4f}".format(
-                    epoch, args.epochs, iteration, len(data_loader)-1, loss.item()))
+                logContent = "Epoch {:02d}/{:02d} Batch {:04d}/{:d}, Loss {:9.4f}".format(
+                    epoch, args.epochs, iteration, len(data_loader)-1, loss.item())
+                print(logContent)
+                with open(logPath, 'a') as log:
+                    for arg in vars(args):
+                        log.write(logContent + "\n")
 
                 if args.conditional:
                     c = torch.arange(0, 10).long().unsqueeze(1)
@@ -108,9 +114,11 @@ def main(args):
                 plt.close('all')
 
         df = pd.DataFrame.from_dict(tracker_epoch, orient='index')
+
         g = sns.lmplot(
             x='x', y='y', hue='label', data=df.groupby('label').head(100),
             fit_reg=False, legend=True)
+
         g.savefig(os.path.join(
             args.fig_root, str(ts), "E{:d}-Dist.png".format(epoch)),
             dpi=300)
@@ -125,11 +133,19 @@ if __name__ == '__main__':
     parser.add_argument("--learning_rate", type=float, default=0.001)
     parser.add_argument("--encoder_layer_sizes", type=list, default=[784, 256])
     parser.add_argument("--decoder_layer_sizes", type=list, default=[256, 784])
-    parser.add_argument("--latent_size", type=int, default=2)
+    parser.add_argument("--latent_size", type=int, default=1)
     parser.add_argument("--print_every", type=int, default=100)
     parser.add_argument("--fig_root", type=str, default='ae/figs')
     parser.add_argument("--conditional", action='store_true')
 
     args = parser.parse_args()
 
-    main(args)
+    ts = time.time()
+
+    logPath = os.path.join(args.fig_root, str(ts), 'log.txt')
+    os.makedirs(os.path.dirname(logPath))
+    with open(logPath, 'a') as log:
+        for arg in vars(args):
+            log.write(arg + ": " + str(getattr(args, arg)) + "\n")
+
+    main(args, ts, logPath)
